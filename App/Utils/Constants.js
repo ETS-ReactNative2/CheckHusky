bf10d6405c748a7c7b8b230dedb83da9aaac768e
Hanuman <hanuman@systango.com>
2019-10-03 16:58:15 +0530

    added all basic dummy constants

--- App/Utils/Constants.js
+++ App/Utils/Constants.js
@@ -1,3 +1,47 @@
+// Screen Constatnts
+export const SCREEN_HEIGHT = 667;
+export const SCREEN_WIDTH = 375;
+
+// Color Constants
+export const WHITE_COLOR = '#FFFFFF';
+export const BLACK_COLOR = '#000000';
+export const GREY_COLOR = '#626262';
+export const STATUS_BAR_BACKGROUND_COLOR = '#FFFFFF';
+export const ACTIVE_TAB_COLOR = '#457ced';
+export const TITLEBAR_BACKGROUND_COLOR = 'rgb(253,249,249)';
+export const BORDER_COLOR_GREY = 'rgb(208,208,208)';
+export const BORDER_COLOR_GREY_LIGHT = 'rgb(221,221,221)';
+export const BORDER_SHADOW_COLOR = 'rgba(0,0,0,0.03)';
+export const BLUE_COLOR_COLLAPSE = 'rgb(69,124,237)';
+export const ICON_BG = 'rgb(253, 249, 249)';
+export const LIGHT_GREY_BG = 'rgb(235, 235, 235)';
+
+// Font Weight Constants
+export const fontWeight = {
+  Thin: '100',
+  UltraLight: '200',
+  Light: '300',
+  Regular: '400',
+  Medium: '500',
+  Semibold: '600',
+  Bold: '700',
+  Heavy: '800',
+  Black: '900'
+};
+
+// Font Family constants 'TODO custom fonts needs to install'
+export const fontFamily = {
+  Regular: 'Raleway-Regular',
+  Medium: 'Raleway-Medium',
+  SemiBold: 'Raleway-SemiBold',
+  Light: 'Raleway-Light',
+  Bold: 'Raleway-Bold',
+  MediumItalic: 'Raleway-MediumItalic',
+  RegularItalic: 'Raleway-Italic',
+  Black: 'Raleway-Black'
+};
+
+// Saga Constants
 export const START_UP = 'START_UP';
 
 export const USER_LOGIN = 'USER_LOGIN';
@@ -11,3 +55,14 @@ export const USER_LOGOUT_FAILURE = 'USER_LOGOUT_FAILURE';
 export const USER_LOGGED_OUT_SUCCESSFULLY = 'USER_LOGGED_OUT_SUCCESSFULLY';
 export const ERROR_IN_LOGOUT = 'ERROR_IN_LOGOUT';
 export const CHANGE_LANGUAGE = 'CHANGE_LANGUAGE';
+
+// Messages
+export const MSG_TITLE = 'Any Message Title';
+
+// image constants 'TODO specify correct assets path'
+export const HOME_INACTIVE = require('@assets/');
+export const HOME_ACTIVE = require('@assets/');
+
+// Screen uri constants
+export const BONUS_URI = 'https://www.xyz.com/app/bonus/';
+export const FAQS_URI = 'https://www.xyz.com/app/faq/';
